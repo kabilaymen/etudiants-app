a6aa959cdffd35a7de7612b4d9cb1d5711c1b3b2
Kabil-Aymen <gjourno738@gmail.com>
2024-12-29 17:12:00 +0100

    added k8s manifests and backend updates - (db credentials in for testing)

--- etudiants-backend/src/main/java/com/example/etudiantsbackend/controllers/EtudiantController.java
+++ etudiants-backend/src/main/java/com/example/etudiantsbackend/controllers/EtudiantController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/etudiants")
+@RequestMapping("/api/etudiants")
 public class EtudiantController {
 
     private final EtudiantService etudiantService;
--- etudiants-frontend/src/api.js
+++ etudiants-frontend/src/api.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
-
 const axiosPlain = axios.create({
-    baseURL: API_URL,
+    baseURL: '/api',
     headers: {
       'Content-Type': 'text/plain',
     },
 });
 
 const axiosJson = axios.create({
-    baseURL: API_URL,
+    baseURL: '/api',
     headers: {
       'Content-Type': 'application/json',
     },
 });
 
-
 export const getEtudiants = () => axiosJson.get('/etudiants');
 export const getEtudiant = (id) => axiosJson.get(`/etudiants/${id}`);
 export const addEtudiant = (nom) => axiosPlain.post('/etudiants', nom);
 export const getNotes = (idEtudiant) => axiosJson.get(`/etudiants/${idEtudiant}/notes`);
-export const addNote = (idEtudiant, note) => axiosJson.post(`etudiants/${idEtudiant}/notes`, note);
+export const addNote = (idEtudiant, note) => axiosJson.post(`/etudiants/${idEtudiant}/notes`, note);
